80bf593068b5aac770137565756b6d338c3c549c
mshofadev <mshofa09@gmail.com>
2025-04-22 17:34:51 +0700

    chore: update database credentials in config

--- db.py
+++ db.py
@@ -3,7 +3,7 @@ import mysql.connector
 def connect_db():
     return mysql.connector.connect(
         host="localhost",
-        user="admingriffith",
-        password="Gr1ff1th@",
+        user="",
+        password="",
         database="face_recognition"
     )
